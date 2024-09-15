09e20413e5ebf284f3d54a07f8789aea8c91cbac
maayan2299 <154839882+maayan2299@users.noreply.github.com>
2024-09-15 13:55:32 +0300

    Add files via upload

---
+++ process_file.h
@@ -0,0 +1,44 @@
+#ifndef _PROCESS_FILE_H
+#define _PROCESS_FILE_H
+#include "writefiles.h"
+#include "utils.h"
+#include "first_pass.h"
+#include "second_pass.h"
+#include "macr.h"
+
+
+
+
+
+
+/**
+ * @brief Processes the specified assembly file by performing macro expansion, first pass and second pass processing,
+ *        and writing output files.
+ * 
+ * @param filename The name of the assembly file to be processed.
+ * @return true if the processing is successful, false otherwise.
+ */
+
+
+
+
+
+ bool process_file(char *filename);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
